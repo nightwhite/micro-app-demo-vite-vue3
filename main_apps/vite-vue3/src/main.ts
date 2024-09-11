9e1400371b7539ddcff7afd20aa4832f0f626d93
nightwhite <nightwhite634@gmail.com>
2024-09-11 23:21:59 +0800

    fix

--- main_apps/vite-vue3/src/main.ts
+++ main_apps/vite-vue3/src/main.ts
@@ -4,43 +4,60 @@ import router from './router'
 import microApp from '@micro-zoe/micro-app'
 
 microApp.start({
+  "router-mode": "pure",
   plugins: {
     modules: {
-      'appname-vite': [
+      "appname-vite": [
         {
           loader(code) {
-            if (process.env.NODE_ENV === 'development') {
-              // 这里 /basename/ 需要和子应用vite.config.js中base的配置保持一致
-              code = code.replace(/(from|import)(\s*['"])(\/child\/vite\/)/g, all => {
-                return all.replace('/child/vite/', 'http://localhost:4007/child/vite/')
-              })
+            if (process.env.NODE_ENV === "development") {
+              // 这里 /basename/ 需要和子应用 vite.config.js 中 base 的配置保持一致
+              code = code.replace(
+                /(from|import)(\s*['"])(\/child\/vite\/)/g,
+                (all) => {
+                  return all.replace(
+                    "/child/vite/",
+                    "http://localhost:4007/child/vite/"
+                  );
+                }
+              );
             }
 
-            return code
-          }
-        }
+            return code;
+          },
+        },
       ],
-      // 解决create-react-app中sockjs-node报错的问题
-      'appname-react16': [{
-        loader(code) {
-          if (process.env.NODE_ENV === 'development' && code.indexOf('sockjs-node') > -1) {
-            code = code.replace('window.location.port', '4004')
-          }
-          return code
-        }
-      }],
-      // 解决create-react-app中sockjs-node报错的问题
-      'appname-react17': [{
-        loader(code) {
-          if (process.env.NODE_ENV === 'development' && code.indexOf('sockjs-node') > -1) {
-            code = code.replace('window.location.port', '4005')
-          }
-          return code
-        }
-      }],
-    }
-  }
-})
+      // 解决 create-react-app 中 sockjs-node 报错的问题
+      "appname-react16": [
+        {
+          loader(code) {
+            if (
+              process.env.NODE_ENV === "development" &&
+              code.indexOf("sockjs-node") > -1
+            ) {
+              code = code.replace("window.location.port", "4004");
+            }
+            return code;
+          },
+        },
+      ],
+      // 解决 create-react-app 中 sockjs-node 报错的问题
+      "appname-react17": [
+        {
+          loader(code) {
+            if (
+              process.env.NODE_ENV === "development" &&
+              code.indexOf("sockjs-node") > -1
+            ) {
+              code = code.replace("window.location.port", "4005");
+            }
+            return code;
+          },
+        },
+      ],
+    },
+  },
+});
 
 const app = createApp(App)
 
